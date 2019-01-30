004f551dab5ae2ae625e40dfc64f34fca9d71ef7
Lian Chen <chenlianllik@gmail.com>
2019-01-30 15:22:48 -0800

    offset parser for new FW wlm stats report

--- glam/offset_parser.py
+++ glam/offset_parser.py
@@ -2,7 +2,7 @@ import csv
 
 def wlm_offset_parser(csv_file_name):
 	offset_map_dict = {}
-	cur_offset = 1
+	cur_offset = 0
 	struct_array_num = 1
 	struct_size = 0
 	with open(csv_file_name) as csv_file:
@@ -11,12 +11,12 @@ def wlm_offset_parser(csv_file_name):
 			if 'WMITLV_TAG_STRUC' in row[0]:
 				struct_size = int(row[1][-4:], 16)
 				struct_array_num = int(row[2])
-				struct_size = struct_size/4 + 1
+				struct_size = struct_size/4
 				
 			else:
 				offset_map_dict[row[0]] = [cur_offset + i*struct_size for i in xrange(struct_array_num) ]
+				cur_offset+=1
 			
-			cur_offset+=1
 	print offset_map_dict
 	return offset_map_dict
 if __name__ == '__main__':
