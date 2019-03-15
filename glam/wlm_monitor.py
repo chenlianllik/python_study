46eb490e6f92d162006e15392e5e1bd39cdba3bd
Lian Chen <chenlianllik@gmail.com>
2019-03-15 13:06:57 -0700

    remove event lib as it's not supported on latest Dash version

--- glam/wlm_monitor.py
+++ glam/wlm_monitor.py
@@ -1,5 +1,5 @@
 import dash
-from dash.dependencies import Input, Output, Event, State
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
--- glam/wlm_parser.py
+++ glam/wlm_parser.py
@@ -3,7 +3,7 @@ import datetime
 import io
 
 import dash
-from dash.dependencies import Input, Output, Event, State
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 import dash_table
